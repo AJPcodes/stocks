9746ffce8e87f84e62a21d4e412974adc382ad44
AJP <allenatmc@gmail.com>
2016-02-17 18:41:33 -0600

    change portfolio to show same stocks together

--- services/portfolio.js
+++ services/portfolio.js
@@ -2,13 +2,39 @@
 
 const Stock = require('../models/Stock');
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 module.exports=function(res){
 
   Stock.find()
-    .then((data) => {
-      console.log(data)
-      res.send(data);
+    .then((allPurchases) => {
+
+      //get all the symbols owned
+      let stocksByTicker = _.unionBy(allPurchases, 'symbol').map(function(obj){
+        return _.pick(obj, ['symbol', 'name']);
+      });
+
+      //add all past purchases together
+      stocksByTicker.map(function(obj){
+        obj.quantity = 0;
+        obj.cost = 0;
+
+        for (let i=0; i< allPurchases.length; i++) {
+          let purchase = allPurchases[i];
+
+          if (purchase.symbol === obj.symbol) {
+            obj.quantity += purchase.quantity;
+            obj.cost += purchase.cost;
+          }
+
+        }
+
+        obj.cost = obj.cost.toFixed(2);
+        return obj;
+
+      });
+
+      res.send(stocksByTicker);
   })
 }
 
